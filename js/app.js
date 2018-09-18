7ca948b889f3b275c2b303c0849c51481fbeb1b7
Elisha <EHarmison@spartaglobal.com>
2018-09-18 12:06:02 +0100

    js with solution

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 //if all correct - print back a message
 $(document).ready(function(){
 
+//take the id of infobutton and when clicked run the information function
   $("#infoButton").click(Information);
 
   // Information();
@@ -20,6 +21,32 @@ $(document).ready(function(){
   }
   //Check the input of the userinput
 
+//run through each box, the correct answer is checked
+  var solution = [[1,3,5,8,2,7,6,9,4],
+                [8,6,7,3,4,9,5,1,2],
+                [2,4,9,5,6,1,8,7,3],
+                [5,7,3,4,1,9,2,8,6],
+                [9,8,1,6,2,5,7,3,4],
+                [4,2,6,3,8,7,9,1,5],
+                [7,6,8,9,5,1,3,4,2],
+                [2,5,3,4,7,8,1,9,6],
+                [1,9,4,6,3,2,7,5,8]]
+checkInput();
+
+    function checkInput(){
+      if (solution[0,0] == 1) {
+        $(this).addClass("correct");
+      }
+      else {
+        $(this).addClass("wrong");
+      }
+      if (solution[0][2] == 5){
+        $(this).addClass("correct");
+      }
+      else {
+        $(this).addClass("wrong");
+      }
+    }
 
 
 
@@ -27,8 +54,6 @@ $(document).ready(function(){
 
 
 
-
-  
     //
     // $("#submit").click(function(){
     //   checkInput();
